2b5f1ef1640fdb89a0892186d5b633665ec887d2
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2024-12-20 00:26:28 -0500

    Fixed null pointer if no tests in scheduler

--- src/main/java/com/frc/robot/unittest/UnitTestSequence.java
+++ src/main/java/com/frc/robot/unittest/UnitTestSequence.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
@@ -37,8 +38,12 @@ public class UnitTestSequence extends Command {
             .andThen(new WaitCommand(1.0));
         }
 
-        this.testCommand = new SequentialCommandGroup(commandArray)
-            .until(() -> tests.get(tests.size()-1).isFinished());
+        if (commandArray.length != 0) {
+            this.testCommand = new SequentialCommandGroup(commandArray)
+                .until(() -> tests.get(tests.size()-1).isFinished());
+        } else {
+            this.testCommand = Commands.none();
+        }
     }
 
     @Override
