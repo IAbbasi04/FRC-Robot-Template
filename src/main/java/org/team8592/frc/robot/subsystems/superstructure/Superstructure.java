860a92d14529111b5ee1ad7e3a5947fb8ede7c27
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2025-04-24 17:11:15 -0400

    WIP

--- src/main/java/org/team8592/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/org/team8592/frc/robot/subsystems/superstructure/Superstructure.java
@@ -1,15 +1,20 @@
 package org.team8592.frc.robot.subsystems.superstructure;
 
+import org.team8592.frc.robot.Suppliers;
+
 public class Superstructure {
     // TODO - Put mech2d here
     public enum States {
         ;
 
         public double wristDegrees, shoulderDegrees, elevatorInches;
-        private States(double wrist, double shoulder, double elevator){
-            this.wristDegrees = wrist;
-            this.shoulderDegrees = shoulder;
-            this.elevatorInches = elevator;
+        private States(double compWrist, double compShoulder, double compElevator, double pracWrist, double pracShoulder, double pracElevator){
+            // if (Suppliers.)
+            this.wristDegrees = compWrist;
+            this.shoulderDegrees = compShoulder;
+            this.elevatorInches = compElevator;
+
+
         }
     }
 }
