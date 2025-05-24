338c74e3dac8502b4ab192f337d7fcded41c538b
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2025-05-23 23:32:23 -0400

    Fixed

--- src/main/java/lib/team1731/io/RotationalSubsystemIO.java
+++ src/main/java/lib/team1731/io/RotationalSubsystemIO.java
@@ -2,20 +2,25 @@ package lib.team1731.io;
 
 public abstract class RotationalSubsystemIO implements ISubsystemIO {
     protected double desiredDegrees = 0.0;
+    protected double degreeTolerance = 0.0;
+
+    protected RotationalSubsystemIO(double degreeTolerance) {
+        this.degreeTolerance = degreeTolerance;
+    }
 
     public abstract void setDegrees(double degrees);
 
     public abstract double getDegrees();
 
-    public abstract boolean atAngleDegrees();
+    public boolean atAngleDegrees() {
+        return atAngleDegrees(getDesiredDegrees());
+    }
 
-    public abstract boolean atAngleDegrees(double targetDegrees);
+    public boolean atAngleDegrees(double targetDegrees) {
+        return Math.abs(getDegrees() - targetDegrees) < degreeTolerance;
+    }
 
     public double getDesiredDegrees() {
         return this.desiredDegrees;
-    }
-
-    protected boolean atAngleDegrees(double degrees, double tolerance) {
-        return Math.abs(getDegrees() - degrees) < tolerance;
     }
 }
